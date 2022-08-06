b331af81ae12753c0b229fe30a56d19400ae1f3c
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 01:31:29 -0600

    automatically install all previous brew installation

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#install all brew installations to new computer
+cat homebrew.txt | xargs brew install
+
 # install packer for nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 #install nerd font
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
+
