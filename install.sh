3a92e4b259771d504aa35fc2aa2e8766a19f1748
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 01:12:56 -0600

    edit install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 #install nerd font
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
-defaults write com.googlecode.iterm2 "Normal Font" -string "Hack Nerd Font"
-defaults write com.googlecode.iterm2 "Non Ascii Font" -string "Hack Nerd Font"
