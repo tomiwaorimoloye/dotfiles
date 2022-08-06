33f71cef70f07689055a3862a1b00462ddbc7d63
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 00:15:33 -0600

    Add nerd font to install script

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 # install packer for nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+#install nerd font
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
