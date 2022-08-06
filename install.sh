b1e083161894cd185ac77e63b5917e2700980187
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 01:55:57 -0600

    test brew installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-#install all brew installations to new computer
-cat homebrew.txt | xargs brew install
+# check if homebrew is installed
+which brew
+if [[ $? == "brew not found" ]] ; then
+  echo 'brew not found'
+  #install homebrew
+#  cd /usr/local/ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+  #brew bundle install
+else
+  echo 'brew found'
+  #brew update
+  #brew bundle install
+fi
 
 # install packer for nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
