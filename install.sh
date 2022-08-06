1f509326e858386a85a9b32154a98cda39228070
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 15:14:33 -0600

    Modify install and update function depending on arg

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # install homebrew and all the brew installations
-which brew
-if [[ $? == "brew not found" ]] ; then
-  echo 'brew not found'
-  install homebrew
-  cd /usr/local/ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-  brew bundle install
-else
-  echo 'brew found'
-  brew update
-  brew bundle install
-fi
+function install() {
+  which brew
+  if [[ $? == "brew not found" ]] ; then
+    echo 'brew not found'
+    install homebrew
+    cd /usr/local/ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+    brew bundle install
+  else
+    echo 'brew found'
+    brew update
+    brew bundle install
+  fi
 
-# install packer for nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  # install packer for nvim
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-#install nerd font
-brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
-
+  #install nerd font
+  brew tap homebrew/cask-fonts
+  brew install --cask font-hack-nerd-font
+}
 
 
 # this will update global modules to global_npm  & brewfiles and then you should 
@@ -38,5 +39,7 @@ function updateLocal() {
 
 if [[ $1 == "update" ]]
 then
-  updateLocal()
+  updateLocal
+else
+  install
 fi
