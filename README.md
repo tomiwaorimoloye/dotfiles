27f6945eebb1241a5d11030d9870ee45e11255c2
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-08-06 16:33:46 -0600

    add readme and refactor install.sh

---
+++ README.md
@@ -0,0 +1,4 @@
+# First update your main computer .dotfiles folder by running bash install bash install.sh update
+# then update your remote repo by committing the edited files and push it to the remote
+# then update your ~/.dotfiles in your new computer from the remote repo and run bash install.sh remote
+
--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# install homebrew and all the brew installations
 function install() {
+  # install homebrew and all the brew installations
   which brew
   if [[ $? == "brew not found" ]] ; then
     echo 'brew not found'
-    install homebrew
+    # install homebrew
     cd /usr/local/ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
     brew bundle install
   else
     echo 'brew found'
+    # update your brew and add new installations
     brew update
     brew bundle install
   fi
@@ -20,26 +21,32 @@ function install() {
   #install nerd font
   brew tap homebrew/cask-fonts
   brew install --cask font-hack-nerd-font
+
+  # install global node modules which include lsp servers
+  bash global_npm.sh
 }
 
 
 # this will update global modules to global_npm  & brewfiles and then you should 
 # git add and commit on this computer and then git push
-# then git pull and run global_npm.sh script before running ./install.sh on the new computer
+# then git pull and run bash install.sh on the new computer
 function updateLocal() {
-  # install global node_modules and lspconfigs servers
+  # create a file (global_npm.sh) that installs global node_modules and lspconfigs servers
   npm list -g --depth=0 > global_npm.txt && sed -i '' 's/├── //g' global_npm.txt && sed -i '' 's/└── //g' global_npm.txt && sed -i '' 's/\/usr\/local\/lib//g' global_npm.txt && sed -i '' '/^[[:space:]]*$/d' global_npm.txt && rm global_npm.sh; touch global_npm.sh && cat global_npm.txt | while read line; do echo "npm install -g "${line} >> global_npm.sh; done && chmod 755 global_npm.sh && rm global_npm.txt
   
-  # update brew
+  # update brew and installations
   brew update
-
-  #update brew installations
-  brew bundle dump
+  mv ./Brewfile ./old-brewfile.txt
+  brew bundle dump # this creates a new ./Brewfile
 }
 
-if [[ $1 == "update" ]]
+# this is the conditional that determines whether to update the .dotfiles folder and sync to github
+if [ $1 == "update" ]
 then
+  echo "updating local..."
   updateLocal
-else
+elif [ $1 == "remote" ]
+then
+  echo "installing from remote..."
   install
 fi
